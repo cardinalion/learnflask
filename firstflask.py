bceccc70434914021a57b2150a4cdf31510c53e7
Wang <wangnyu@amazon.com>
2019-01-21 19:19:13 +0800

    route & debug

--- firstflask.py
+++ firstflask.py
@@ -8,4 +8,18 @@ __author__ = 'cardinalion'
 
 app = Flask(__name__)
 
-app.run()
+"""
+#another url registration method
+app.add_url_rule('/hello',view_func=hello)
+#基于类的视图（即插视图）
+"""
+
+#'/hello/' redirects to '/hello', with status number 301,302, but doesn't have unique url
+#click CLTL + B to go to the source code
+#route function also uses app.add_url_rule('',)
+@app.route('/hello')
+def hello():
+    return 'Hello'
+
+#debug mode on，no need to restart server，detailed error info
+app.run(debug=True)
